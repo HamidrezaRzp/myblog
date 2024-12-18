8cebc1ab7f1966009061c7c5ca94d58abdc56837
pineapplejuice <hamidreza.bigred3@gmail.com>
2024-12-19 02:19:36 +0330

    post model created and migrated to db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering =['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
+    
