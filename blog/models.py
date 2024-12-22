fdde14a562e6b859b11551ef82ae0364604cbea3
pineapplejuice <hamidreza.bigred3@gmail.com>
2024-12-22 04:04:39 +0330

    status and users foriegn key added to post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        pUBLISHED = 'Published' ,'PB'
+        DRAFTED = 'drafted' , 'DF'
     
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
@@ -9,6 +15,17 @@ class Post(models.Model):
     created = models.DateTimeField(default=timezone.now)
     publish = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+            max_length=2,
+            choices=Status,
+            default=Status.DRAFTED
+    )
+    authur = models.ForeignKey(
+                settings.AUTH_USER_MODEL,
+                related_name='post',
+                on_delete=models.CASCADE
+    )
+
 
     class Meta:
         ordering =['-publish']
